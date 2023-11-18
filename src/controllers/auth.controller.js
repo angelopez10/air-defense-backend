e740a6e5f521c9287dd8c9fedfd98a38be2b45c5
Angel <angelopez10898@gmail.com>
2023-11-18 19:59:36 -0300

    login crud

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,66 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
+
 export const register = async (req, res) => {
-  res.send("registrando");
-  console.log(req.body);
+  const { rut, fullName, phoneNumber, address, email, password } = req.body;
+
+  try {
+    const encryptedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      rut,
+      fullName,
+      phoneNumber,
+      address,
+      email,
+      password: encryptedPassword,
+    });
+
+    const userSaved = await newUser.save();
+
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      rut: userSaved.rut,
+      fullName: userSaved.fullName,
+      phoneNumber: userSaved.phoneNumber,
+      address: userSaved.address,
+      email: userSaved.email,
+      isAdmin: userSaved.isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 export const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  try {
+    const userFound = await User.findOne({ email });
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const matchPassword = await bcrypt.compare(password, userFound.password);
+
+    if (!matchPassword)
+      return res.status(401).json({ message: "Invalid password" });
+
+    const token = await createAccessToken({ id: userFound._id });
+
+    res.cookie("token", token);
+    res.json({
+      id: userFound._id,
+      rut: userFound.rut,
+      fullName: userFound.fullName,
+      phoneNumber: userFound.phoneNumber,
+      address: userFound.address,
+      email: userFound.email,
+      isAdmin: userFound.isAdmin,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,37 +1,42 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  rut: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    rut: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  fullName: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
