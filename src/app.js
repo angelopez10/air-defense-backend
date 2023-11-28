c663a216088588c52f2d85711406f52863b31f89
Angel <angelopez10898@gmail.com>
2023-11-28 19:55:49 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors(
-   {origin: "http://localhost:5173",  credentials: true}
-));
+app.use(cors({ credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
