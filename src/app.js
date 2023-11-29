0ec4ead7325e09fba7969ecd1a4ded71b22a7ed1
Angel <angelopez10898@gmail.com>
2023-11-28 21:25:41 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors({ origin: "https://defensaerea.cl", credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
